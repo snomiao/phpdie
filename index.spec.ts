6f959e64363bdfbfb893eef50ba866906e97ce1e
snomiao <snomiao@gmail.com>
2024-07-02 17:39:54 +0900

    fix(main): add test
    add test

--- index.spec.ts
+++ index.spec.ts
@@ -27,3 +27,14 @@ it("dies with error", () => {
   }
   expect(err.message).toEqual("Missing Token");
 });
+
+it("dies with template string", () => {
+  let err: any;
+  try {
+    const token = process.env.TOKEN ?? DIE`Missing Token`;
+    console.log(token);
+  } catch (e) {
+    err = e;
+  }
+  expect(err).toEqual("Missing Token");
+});
