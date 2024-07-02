6184043fee91fa228cebcaa690642cfaa4be86e2
snomiao <snomiao@gmail.com>
2024-07-02 17:13:36 +0900

    fix(die): support array
    support array

--- index.ts
+++ index.ts
@@ -1,10 +1,20 @@
-export default function DIE(reason?: string | Error): never {
+export default function DIE(
+  reason?: string | ReadonlyArray<string> | Error,
+  ...slots: string[]
+): never {
   if (typeof reason === "string") {
     throw reason.trim();
+  }
+  if (Array.isArray(reason)) {
+    throw reason.map((e, i) => e + (slots[i] ?? "")).join("");
   }
   throw reason;
 }
 export function DIEAlert(reason?: string | Error): never {
-  alert(String(reason instanceof Error && reason.message || reason))
-  DIE(reason)
+  alert(String((reason instanceof Error && reason.message) || reason));
+  DIE(reason);
+}
+export function DIEProcess(reason?: string | Error): never {
+  console.error(reason);
+  process.exit(1);
 }
