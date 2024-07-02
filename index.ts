9953a0812a2b99b7bf11f58950ea8faccb90db08
snomiao <snomiao@gmail.com>
2024-07-02 08:29:38 +0000

    fix(main): roa
    roa

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
-export default function DIE(reason?: string | Error): never {
+type Reason = string | ReadonlyArray<string> | Error;
+export default function DIE(reason?: Reason): never {
   if (typeof reason === "string") {
     throw reason.trim();
   }
   throw reason;
 }
-export function DIEAlert(reason?: string | Error): never {
-  alert(String(reason instanceof Error && reason.message || reason))
-  DIE(reason)
+export function DIEAlert(reason?: Reason): never {
+  alert(String((reason instanceof Error && reason.message) || reason));
+  DIE(reason);
 }
