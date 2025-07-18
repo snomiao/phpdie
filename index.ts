5f9dd1e19e0ee7d1f4583c1edbfaeb153f561210
snomiao <snomiao@gmail.com>
2025-07-18 08:55:17 +0000

    feat(index.ts): add DIES function for error handling with customizable alert options docs(index.ts): update DIEAlert documentation to deprecate it in favor of DIES function

--- index.ts
+++ index.ts
@@ -11,14 +11,34 @@ export function DIEError(reason?: Reason, ...slots: StringLike[]): never {
 export function DIE(reason?: Reason, ...slots: StringLike[]): never {
   throw errorFormat(reason, ...slots);
 }
+/**
+ * Die with string, this is the same as DIE but with a different name.
+ * 
+ * allow use toast.error or alert to show the error message
+ * @param alert - function to show the error message, could be `alert`, `console.error`, `toast.error`, or any other function that accepts a string.
+ */
+export function DIES<Args extends unknown[]>(alert: (...args: readonly [...Args]) => any, ...args: readonly [...Args]): never {
+  alert(...args);
+  throw new Error('DIES', { cause: args });
+}
+
+/**
+ * show an alert with the error message and stop current function.
+ *
+ * @deprecated Use DIES(alert, ...) instead
+ */
 export const DIEAlert: typeof DIE = (...args) => {
   alert(stringifyError(...args));
   DIE(...args);
 };
+
+/**
+ * Node.js process only, print error and exit with code 1.
+ */
 export const DIEProcess: typeof DIE = (...args) => {
   console.error(stringifyError(...args));
   process.exit(1);
-};
+}
 
 function errorFormat(reason?: Reason, ...slots: StringLike[]) {
   if (typeof reason === "string") {
